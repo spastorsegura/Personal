54cc71f285b49b5ec474acdb4b4c974d54d66342
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-04-02 14:47:45 -0500

    subri

--- PRUEBA PHYTON/SEMANA1/colecciones.py
+++ PRUEBA PHYTON/SEMANA1/colecciones.py
@@ -1,13 +1,12 @@
 # Lista
 cursos_back=['python','flask','django']
-# Tuplas
-cursos_back=('python','flask','django')
+# Tuplas: colecciones de datos que no se pueden modificar
+# cursos_back=('python','flask','django')
 # Diccionarios
-cursos_back={'CURSO1:','PYTHON','CURSO2:','django'}
+# cursos_back={'CURSO1:','PYTHON','CURSO2:','django'}
 
 cursos_back.append('NODEJS')
 cursos_back.reverse
 for cursos in cursos_back:
     print(cursos)
-
 print(cursos_back)
