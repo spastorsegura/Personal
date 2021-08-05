ec42a159522066db73de1771ebb682f95831b37d
spastorsegura <67179586+spastorsegura@users.noreply.github.com>
2021-08-05 09:21:46 -0400

    subir

--- EsNatural final/tienda/inventario/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class MostrarProductosSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=ProductoModel
-        fields= '__all__'
